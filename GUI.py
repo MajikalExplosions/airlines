e8741877ffad7983c9eb8f97860fd68c4e9072ed
Chrisotpher Luey <cluey21@csus.org>
2020-05-08 12:05:40 -0700

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,16 @@
+# File: 
+# Written By: Christopher Luey
+# Date: 
+# 
+
+from graphics import *
+
+class GUI:
+
+    def __init__(self):
+        pass
+
+class Screen:
+
+    def __init__(self):
+        pass

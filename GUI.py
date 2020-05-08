b7f293e8a0d18b148c5a70fe759a169e56ed0393
Chrisotpher Luey <cluey21@csus.org>
2020-05-08 12:52:17 -0700

    Create GUI.py

--- GUI.py
+++ GUI.py
@@ -1,16 +1,46 @@
 # File: 
 # Written By: Christopher Luey
 # Date: 
-# 
+#
 
 from graphics import *
 
 class GUI:
 
     def __init__(self):
+        self.win = GraphWin(title="Airport", width=1000, height=800, autoflush=False)
+        main = Screen('main')
+        self.activeScreen = main
         pass
+
+    def switchScreen(self, screen):
+        if screen == "MAIN":
+            pass
+        elif screen == "CREATE_RESERVATION":
+            pass
+        elif screen == "MODIFY_RESERVATION":
+            pass
+        elif screen == "FLIGHT_STATUS":
+            pass
+        elif screen == "CHECKIN":
+            pass
+        elif screen == "START":
+            pass
+        else:
+            raise("unknown screen")
+
+    def getScreen(self):
+        return self.activeScreen
+
+    def setButtonClickListener(self):
+
 
 class Screen:
 
-    def __init__(self):
+    def __init__(self, name, win):
+        self.name = name
+        self.win = win
         pass
+
+    def undraw(self):
+

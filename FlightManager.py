b1591f54c269947d12985250352cd37ee20cf58a
Kyler <kylerrosen@gmail.com>
2020-05-08 12:56:01 -0700

    ye

--- FlightManager.py
+++ FlightManager.py
@@ -1,9 +1,22 @@
 class FlightManager:
 	def __init__(self,file):
-		pass
+		file = open(file,"r")
+
+		flights = file.readlines()[1:]
+
+		print(flights[2])
+
+		file.close()
 
 	def getFlights(self):
 		pass
 
 	def getFlightsFrom(self):
 		pass
+
+
+def test():
+	FlightMan = FlightManager("Flight Data - All Flights.csv")
+
+if __name__ == '__main__':
+	test()

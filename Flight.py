30756c14e7064f81bc6c4ac34f966c7036db7bed
Kyler <kylerrosen@gmail.com>
2020-05-08 12:19:47 -0700

    Create Flight.py

---
+++ Flight.py
@@ -0,0 +1,30 @@
+class Flight:
+	def __init__(self,id, airline, num, origin, destination, depTime, arrTime, days):
+		self.id = id 
+        self.airline = airline
+        self.number = num
+        self.orig = origin
+        self.dest = destination
+        self.depTime = depTime
+        self.arrTime = arrTime
+
+    def getID(self):
+    	return self.id
+
+    def getAirline(self):
+    	return self.airline
+        
+    def getNumber(self):
+        return self.number
+
+    def getOrigin(self):
+        return self.orig
+
+    def getDestination(self):
+        return self.dest
+
+    def getDepTime(self):
+        return self.depTime
+
+    def getArrTime(self):
+        return self.arrTime

2fbc25f6500ad617676ee35215f561c78b6698e9
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-05-15 09:35:09 -0700

    Finish up airport and flight

--- Airport.py
+++ Airport.py
@@ -1,8 +1,22 @@
+# Name: Airport.py
+# Description: Stores data for a single airport
+
+# Ver.	Writer			    Date			Notes
+# 0.1   Kyler Rosen         05/09/20		Add airport code, city, and timezone
+# 0.2   Joseph Liu			05/15/20		Add flight data storage for graph
+
 class Airport:
-	def __init(self,code, city, timezone):
+	def __init(self, code, city, timezone):
 		self.code = code
 		self.city = city
 		self.timezone = timezone
+		self.flights = []
+
+	def addFlight(self, f):
+		self.flights.append(f)
+
+	def getFlights(self):
+		return self.flights
 
 	def getCode(self):
 		return self.code
--- Flight.py
+++ Flight.py
@@ -1,15 +1,38 @@
+# Name: Flight.py
+# Description: Stores data for a single flight
+
+# Ver.	Writer			    Date			Notes
+# 0.1   Kyler Rosen         05/09/20		Add flight info from file except days
+# 0.2   Joseph Liu			05/15/20		Renaming and add days
+
 class Flight:
-	def __init__(self, id, airline, num, origin, destination, depTime, arrTime, days):
-		self.id = id 
+	def __init__(self, i, airline, num, origin, destination, depTime, arrTime, days):
+        #Python has built-in ids, so we can't use the self.id property
+		self.flightId = i
         self.airline = airline
         self.number = num
         self.origin = origin
         self.destination = destination
         self.departureTime = depTime
         self.arrivalTime = arrTime
+        
+        #Add days where flight is run
+        if days == "Daily":
+            self.runsOn = [True, True, True, True, True, True, True]
+        else:
+            #Not daily flight
+            if days[0] == "X":
+                self.runs = [True, True, True, True, True, True, True]
+                days = days[1:]
+            else:
+                self.runs = [False, False, False, False, False, False, False]
+            
+            #For each
+            for i in range(len(days)):
+                self.runsOn[int(days[i]) - 1] = not self.runsOn[int(days[i]) - 1]
 
     def getID(self):
-    	return self.id
+    	return self.flightId
 
     def getAirline(self):
     	return self.airline
@@ -18,13 +41,13 @@ class Flight:
         return self.number
 
     def getOrigin(self):
-        return self.orig
+        return self.origin
 
     def getDestination(self):
-        return self.dest
+        return self.destination
 
     def getDepTime(self):
-        return self.depTime
+        return self.departureTime
 
     def getArrTime(self):
-        return self.arrTime
+        return self.arrivalTime

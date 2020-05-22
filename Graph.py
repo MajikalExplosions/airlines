23eb9b034ae2173ed18355ba5a76c96dbb88a87f
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-05-22 13:11:13 -0700

    Finish KSP
    Note: not tested yet.

--- Graph.py
+++ Graph.py
@@ -3,35 +3,64 @@
 
 # Ver.	Writer			        Date			Notes
 # 1.0   Joseph Liu              05/15/20		Original
+# 1.1   Joseph Liu              05/20/20		Add edges
 
 import heapq
 
 class Graph:
-    def __init__(self, fm):
+    def __init__(self):
         self.nodes = []
         self.MAX = 1000000000
-        airports = fm.getAirports()
-        for i in range(len(airports)):
-            self.nodes.append(Node(i, airports[i], self.MAX))
+        self.ntoa = {}
+        self.aton = {}
+
+    def fromFlights(self, fm):
+        a = fm.getAirports()
+        for i in range(len(a)):
+            self.nodes.append(Node(i, self.MAX))
+            self.ntoa[i] = a.getId()
+            self.aton[a.getId()] = i
+        
+        for airport in a:
+            for flight in airport.getFlights():
+                self.aton[airport.getId()].addEdge(Edge(flight, airport.getId(), self.aton[flight.getDestination().getId()]))
+
+    def removeEdges(self, remove):
+        for e in remove:
+            self.nodes[e.u].getEdges().remove(e)
     
+    def addEdges(self, add):
+        for e in add:
+            self.nodes[e.u].addEdge(e)
+    
+    def removeNodes(self, remove):
+        for n in remove:
+            self.nodes[n.nid] = 0
+    
+    def addNodes(self, add):
+        for n in add:
+            self.nodes[n.nid] = n
+
     def getNodes(self):
         return self.nodes
+    
+    def exists(self, id):
+        return self.nodes[id] == 0
             
 
 class Node:
-    def __init__(self, i, a, m):
+    def __init__(self, i, m):
         self.nid = i
-        self.airport = a
-        a.setNode(self)
         #Dijkstra stuff
         self.reset(m)
         self.v = False
+        self.edges = []
     
-    def getEdges(self):
-        return self.airport.getFlights()
+    def addEdge(self, e):
+        self.edges.append(e)
 
-    def getAirport(self):
-        return self.airport
+    def getEdges(self):
+        return self.edges
     
     def getDist(self):
         return self.dist
@@ -47,21 +76,27 @@ class Node:
     
     def reset(self, mx):
         self.dist = mx
-        self.flightIn = 0
+        self.edgeIn = 0
         self.v = False
     
-    def setFlightIn(self, p):
-        self.flightIn = p
+    def setEdgeIn(self, p):
+        self.edgeIn = p
 
-    def getFlightIn(self):
-        return self.flightIn
+    def getEdgeIn(self):
+        return self.edgeIn
     
     def isRoot(self):
-        return self.flightIn == -1
+        return self.edgeIn == -1
 
     def root(self):
         self.dist = 0
-        self.flightIn = -1
+        self.edgeIn = -1
 
     def __lt__(self, o):
         return 0
+    
+class Edge:
+    def __init__(self, flight, start, end):
+        self.f = flight
+        self.u = start
+        self.v = end
--- YenKSP.py
+++ YenKSP.py
@@ -1,16 +1,56 @@
-# Name: EppsteinKSP.py
+# Name: YenKSP.py
 # Description: Finds the K shortest paths between two airports using a modified Eppstein's KSP algorithm optimized for small values of K.
 
 # Ver.	Writer			        Date			Notes
 # 1.0   Joseph Liu              05/15/20		Original
 
-import heapq
+from DijkstraSP import *
 
 class YenKSP:
-    def __init__(self, fm, origin, dest, k):
-        self.paths = []
+    def __init__(self, graph, origin, dest, k):
+        A = [DijkstraSP(graph, origin).getPathNode(dest)]
+        B = []
+
+        for pathNum in range(1, k):
+            pathNodes = A[pathNum - 1]
+
+            for i in range(len(A[pathNum - 1]) - 1):
+                spurNode = pathNodes[i]
+                rootPath = pathNodes[:i + 1]
+            
+                removedEdges = []
+                removedNodes = []
+                for path in A:
+                    if rootPath == path.getPathNode(dest)[:i + 1]:
+                        removedEdges.append(graph.getNodes()[i + 1].getEdgeIn())
+                
+                graph.removeEdges(removedEdges)
+                
+                for node in rootPath[:-1]:
+                    removedNodes.append(node)
+                
+                graph.removeNodes(removedNodes)
+
+                spurPath = DijkstraSP(graph, spurNode)
+                
+                totalPath = rootPath + spurPath
+                if totalPath not in B:
+                    B.append(totalPath)
+                
+                graph.addNodes(removedNodes)
+                graph.addEdges(removedEdges)
+
+                if len(B) == 0:
+                    break
+
+                B.sort(key=pathLength)
+                A.append(B[0])
+                B = B[1:]
         
-        #Do some fancy algorithm processing
+        self.paths = A
     
     def getPath(self, k):
         return self.paths[k]
+
+    def pathLength(a):
+        return a[-1].getDist()

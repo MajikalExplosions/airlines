1374ebdf9eb7a7e01b02984007ee5573cd555ce1
Chrisotpher Luey <cluey21@csus.org>
2020-05-24 13:09:49 -0700

    kljh

--- flights/FlightManager.py
+++ flights/FlightManager.py
@@ -10,113 +10,109 @@
 from flights.Airport import Airport
 from flights.Flight import Flight
 
-
 def getFlightNum(airport):
-    return airport.getFlightNum()
-
+	return airport.getFlightNum()
 
 class FlightManager:
-    def __init__(self, airportFile, flightFile):
-        self.flights = []
-        self.airports = []
-        codes = {}
+	def __init__(self, airportFile, flightFile):
+		self.flights = []
+		self.airports = []
+		codes = {}
 
-        af = open(airportFile, "r")
-        contents = af.readlines()[1:]
-        af.close()
-        index = 0
-        for line in contents:
-            l = line.split("\t")
-            for i in range(len(l)):
-                l[i] = l[i].strip()
-            self.airports.append(Airport(l[0], l[3], l[2], l[1], int(l[4])))
-            codes[self.airports[-1].getCode()] = index
-            index += 1
+		af = open(airportFile, "r")
+		contents = af.readlines()[1:]
+		af.close()
+		index = 0
+		for line in contents:
+			l = line.split("\t")
+			for i in range(len(l)):
+				l[i] = l[i].strip()
+			self.airports.append(Airport(l[0], l[3], l[2], l[1], int(l[4])))
+			codes[self.airports[-1].getCode()] = index
+			index += 1
+		
+		ff = open(flightFile, "r")
+		contents = ff.readlines()[1:]
+		ff.close()
+		index = 0
+		for line in contents:
+			l = line.split("\t")
+			for i in range(len(l)):
+				l[i] = l[i].strip()
+			self.flights.append(Flight(index, l[4], int(l[5]), self.airports[codes[l[0]]], self.airports[codes[l[1]]], l[2], l[3], l[6]))
+			self.flights[-1].getOrigin().addFlight(self.flights[-1])
+			self.flights[-1].getDestination().addFlight(self.flights[-1])
+			index += 1
 
-        ff = open(flightFile, "r")
-        contents = ff.readlines()[1:]
-        ff.close()
-        index = 0
-        for line in contents:
-            l = line.split("\t")
-            for i in range(len(l)):
-                l[i] = l[i].strip()
-            self.flights.append(
-                Flight(index, l[4], int(l[5]), self.airports[codes[l[0]]], self.airports[codes[l[1]]], l[2], l[3],
-                       l[6]))
-            self.flights[-1].getOrigin().addFlight(self.flights[-1])
-            self.flights[-1].getDestination().addFlight(self.flights[-1])
-            index += 1
+		self.airports.sort(key = getFlightNum,reverse = True)
 
-        self.airports.sort(key=getFlightNum, reverse=True)
+		for index in range(len(self.airports)):
+			self.airports[index].assignId(index)
+		
+		print("Done processing. Flights:", len(self.flights), "| Airports:", len(self.airports))
+			
 
-        for index in range(len(self.airports)):
-            self.airports[index].assignId(index)
+	def getFlights(self):
+		return self.flights
 
-        print("Done processing. flights:", len(self.flights), "| Airports:", len(self.airports))
+	def getAirports(self):
+		return self.airports
 
-    def getFlights(self):
-        return self.flights
+	def searchAirport(self,string):
+		matchingAirports = []
+		for airport in self.airports:
+			if len(string) == 3 and airport.getCode().lower() == string.lower():
+				matchingAirports.append(airport)
 
-    def getAirports(self):
-        return self.airports
+			if airport.getCity().lower() == string.lower():
+				matchingAirports.append(airport)
 
-    def searchAirport(self, string):
-        matchingAirports = []
-        for airport in self.airports:
-            if len(string) == 3 and airport.getCode().lower() == string.lower():
-                matchingAirports.append(airport)
+			if airport.getName().lower() == string.lower():
+				matchingAirports.append(airport)
 
-            if airport.getCity().lower() == string.lower():
-                matchingAirports.append(airport)
+			if airport.toString().lower() == string.lower():
+				matchingAirports.append(airport)
 
-            if airport.getName().lower() == string.lower():
-                matchingAirports.append(airport)
+			if len(matchingAirports) == 10:
+				return matchingAirports
 
-            if airport.toString().lower() == string.lower():
-                matchingAirports.append(airport)
+		for airport in self.airports:
+			if (len(string) <= len(airport.getCity())) and (airport.getCity()[:len(string)].lower() == string.lower()) and not airport in matchingAirports:
+		 		matchingAirports.append(airport)
 
-            if len(matchingAirports) == 10:
-                return matchingAirports
+			if len(string) <= len(airport.toString()) and airport.toString()[:len(string)].lower() == string.lower() and not airport in matchingAirports:
+				matchingAirports.append(airport)
 
-        for airport in self.airports:
-            if (len(string) <= len(airport.getCity())) and (
-                    airport.getCity()[:len(string)].lower() == string.lower()) and not airport in matchingAirports:
-                matchingAirports.append(airport)
+			if len(matchingAirports) == 10:
+				return matchingAirports
 
-            if len(string) <= len(airport.toString()) and airport.toString()[:len(
-                    string)].lower() == string.lower() and not airport in matchingAirports:
-                matchingAirports.append(airport)
+		return matchingAirports
 
-            if len(matchingAirports) == 10:
-                return matchingAirports
-
-        return matchingAirports
-
-    def getAirport(self, accessNum):
-        return self.airports[accessNum]
+	def getAirport(self,accessNum):
+		return self.airports[accessNum]
 
 
 def test():
-    FlightMan = FlightManager("data/airports.tsv", "data/flights.tsv")
+	FlightMan = FlightManager("data/airports.tsv", "data/flights.tsv")
+	
 
-    print("flights from", FlightMan.airports[0].getName(), "\n")
-    for f in FlightMan.airports[0].getFlights():
-        print(f.toString())
-    print("\n\nflights from", FlightMan.airports[1].getName(), "\n")
-    for f in FlightMan.airports[1].getFlights():
-        print(f.toString())
+	print("Flights from", FlightMan.airports[0].getName(), "\n")
+	for f in FlightMan.airports[0].getFlights():
+	 	print(f.toString())
+	print("\n\nFlights from", FlightMan.airports[1].getName(), "\n")
+	for f in FlightMan.airports[1].getFlights():
+	 	print(f.toString())
 
-    for airport in FlightMan.airports[0:10]:
-        print(airport.toString())
-        print(airport.getFlightNum())
+	for airport in FlightMan.airports[0:10]:
+		print(airport.toString())
+		print(airport.getFlightNum())
 
-    airports = FlightMan.searchAirport("SAN")
 
-    for airport in airports:
-        print(airport.toString())
-        print(airport.getFlightNum())
+	airports = FlightMan.searchAirport("SAN")
 
+	for airport in airports:
+		print(airport.toString())
+		print(airport.getFlightNum())
 
 if __name__ == '__main__':
-    test()
+	test()

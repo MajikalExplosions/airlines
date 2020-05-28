262dd31b5495dfd151dc288e2eca3a1593caac5c
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-05-28 16:22:55 -0700

    Update Actions.py

--- Actions.py
+++ Actions.py
@@ -285,7 +285,7 @@ class ActionManager:
                 self._seatSelectionMode = 1
 
                 totalFlightTime = 0
-                setStartTime(startdate[2], startdate[0], startdate[1])
+                setStartTime(int(startdate[2]), int(startdate[0]), int(startdate[1]))
 
                 for i in range(len(self._currentReservation.getFlights())):
                     nextFlightTime = self._currentReservation.getFlights()[i].timeUntilNextFlight(offsetStartTime(timedelta(hours=totalFlightTime)))

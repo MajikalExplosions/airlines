721a4ea8c58db6e7ad280d93c1d17564bc9dc39d
Shuvam Chatterjee <shuvamc19@gmail.com>
2020-05-28 19:31:01 -0700

    reservation

--- reservations/ReservationManager.py
+++ reservations/ReservationManager.py
@@ -47,13 +47,17 @@ class ReservationManager:
         return reservation
 
     def __reservationMatchesLastName(self, lastName, fileLines, reservationStartInd):
-        passengerLastNameInd = reservationStartInd + 5
+        passengerInd = reservationStartInd
+        while fileLines[passengerInd].find("Passenger") == -1:
+            passengerInd += 1
 
-        while passengerLastNameInd < len(fileLines) and fileLines[passengerLastNameInd].find("Last Name: ") != -1:
-            if fileLines[passengerLastNameInd].lstrip("Last Name: ") == lastName:
+        while fileLines[passengerInd].find("}") == -1:
+            lastNameFile = fileLines[passengerInd].split("  ")[2].lstrip("Last Name: ")
+
+            if lastNameFile == lastName:
                 return True
 
-            passengerLastNameInd += 4
+            passengerInd += 1
 
         return False
 
@@ -73,8 +77,7 @@ class ReservationManager:
                 if confirmationNum == confirmationNumber:
                     #the start of a reservation will be 1 line above where it's confirmation number is
                     return lineNum - 1
-            else:
-                lineNum += 1
+            lineNum += 1
         return -1
 
     def validateCreditCard(self, creditCardNum):

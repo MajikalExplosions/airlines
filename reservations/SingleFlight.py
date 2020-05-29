478eb708bd9cb83d8115c841ba8f9f7c939a56ef
Shuvam Chatterjee <shuvamc19@gmail.com>
2020-05-28 20:21:56 -0700

    reservation

--- reservations/SingleFlight.py
+++ reservations/SingleFlight.py
@@ -116,7 +116,7 @@ class SingleFlight:
         seatList = tokens[9].lstrip("Booked Seats: ")
         for seat in seatList.split(" "):
             row, col = seat.split(",")
-            self.seats[row][col] = True
+            self.seats[row][col] = False
 
     def toString(self):
         string = "Flight"
